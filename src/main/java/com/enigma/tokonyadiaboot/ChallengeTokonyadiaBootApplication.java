49fcfd2cdf621d8c03e843ab4373b604019a8be6
M. Kamil <mk.kamiil@gmail.com>
2021-04-24 10:54:54 +0700

    initialize project tokonyadia-boot

---
+++ src/main/java/com/enigma/tokonyadiaboot/ChallengeTokonyadiaBootApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.tokonyadiaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTokonyadiaBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeTokonyadiaBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/tokonyadiaboot/ChallengeTokonyadiaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.tokonyadiaboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTokonyadiaBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
