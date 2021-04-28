8c56a65d9f9f709b6135446e06da4b52dbfb5099
M. Kamil <mk.kamiil@gmail.com>
2021-04-28 21:43:25 +0700

    add balance validation

---
+++ src/main/java/com/enigma/tokonyadiaboot/dto/WalletDto.java
@@ -0,0 +1,61 @@
+package com.enigma.tokonyadiaboot.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class WalletDto {
+
+    @Id
+    private String id;
+    private String phoneNumber;
+    private BigDecimal balance;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WalletDto walletDto = (WalletDto) o;
+        return Objects.equals(id, walletDto.id) && Objects.equals(phoneNumber, walletDto.phoneNumber) && Objects.equals(balance, walletDto.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, phoneNumber, balance);
+    }
+
+    @Override
+    public String toString() {
+        return "WalletDto{" +
+                "id='" + id + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
